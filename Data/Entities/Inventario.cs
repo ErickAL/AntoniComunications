5dd566803d194c2d1612866985457cb14947cc07
Heriberto Alcantara <HeribertoAlcantara@outlook.com>
2020-07-03 20:09:35 -0400

    initializing project structure

---
+++ Data/Entities/Inventario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnthonyComunication.Shop.Data.Entities
+{
+    public class Inventario
+    {
+    }
+}
